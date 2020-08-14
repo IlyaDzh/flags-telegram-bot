7ba580f7e0cc2ce8cac8b28ea3556a6d092cd053
Илья Долженко <ilay00@mail.ru>
2020-08-14 14:35:56 +0300

    added keyboard

--- src/index.js
+++ src/index.js
@@ -1,16 +1,60 @@
-require("dotenv").config();
+import TelegramBot from "node-telegram-bot-api";
+import { FLAGS } from "./constants";
+import { shuffle } from "./utils";
 
-const TelegramBot = require("node-telegram-bot-api");
+// @FlagsCountryBot
+const Telegram = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+Telegram.onText(/\/start/, sendNewQuestion);
 
-bot.onText(/\/echo (.+)/, function (msg, match) {
-    const fromId = msg.from.id;
-    const resp = match[1];
-    bot.sendMessage(fromId, resp);
+Telegram.on("callback_query", function (query) {
+    const chatId = query.message.chat.id;
+
+    switch (query.data) {
+        case "Yes":
+            Telegram.sendMessage(chatId, "✅ Верный ответ!");
+            break;
+        case "No":
+            Telegram.sendMessage(chatId, "❌ Неверный ответ!");
+            break;
+        default:
+            break;
+    }
+
+    removeKeyboard(query);
+    sendNewQuestion(query.message);
 });
 
-bot.on("message", function (msg) {
+const getRandomCountry = () => {
+    const randIndex = Math.floor(Math.random() * FLAGS.length);
+    return FLAGS[randIndex];
+};
+
+function sendNewQuestion(msg) {
     const chatId = msg.chat.id;
-    bot.sendPhoto(chatId, "public/us.png");
-});
+    const currentCountry = getRandomCountry();
+    const options = {
+        reply_markup: {
+            inline_keyboard: shuffle([
+                [{ text: currentCountry.name, callback_data: "Yes" }],
+                [{ text: getRandomCountry().name, callback_data: "No" }],
+                [{ text: getRandomCountry().name, callback_data: "No" }],
+                [{ text: getRandomCountry().name, callback_data: "No" }]
+            ])
+        }
+    };
+
+    Telegram.sendPhoto(chatId, `public/${currentCountry.code}.png`, options);
+}
+
+const removeKeyboard = query => {
+    Telegram.editMessageReplyMarkup(
+        {
+            inline_keyboard: []
+        },
+        {
+            chat_id: query.from.id,
+            message_id: query.message.message_id
+        }
+    );
+};
